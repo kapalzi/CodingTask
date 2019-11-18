b751578aa42a2ee193bff2cbf03d4df2b5e356bb
krzysztof <235060@student.pwr.edu.pl>
2019-11-18 23:45:32 +0100

    address class

--- CodingTask/Model/Address+CoreDataClass.swift
+++ CodingTask/Model/Address+CoreDataClass.swift
@@ -12,5 +12,25 @@ import CoreData
 
 @objc(Address)
 public class Address: NSManagedObject {
-
+    
+    static func createAddress(country: String, locality: String, street: String, houseNumber: String, flatNumber: String, postcode: String, inContext context: NSManagedObjectContext) {
+        
+        let newAddress = NSEntityDescription.insertNewObject(forEntityName: "Address", into: context) as! Address
+        newAddress.country = country
+        newAddress.locality = locality
+        newAddress.street = street
+        newAddress.houseNumber = houseNumber
+        newAddress.flatNumber = flatNumber
+        newAddress.postcode = postcode
+    }
+    
+    func update(country: String?, locality: String?, street: String?, houseNumber: String?, flatNumber: String?, postcode: String?) {
+        
+        self.country = country ?? self.country
+        self.locality = locality ?? self.locality
+        self.street = street ?? self.street
+        self.houseNumber = houseNumber ?? self.houseNumber
+        self.flatNumber = flatNumber ?? self.flatNumber
+        self.postcode = postcode ?? self.postcode
+    }
 }
